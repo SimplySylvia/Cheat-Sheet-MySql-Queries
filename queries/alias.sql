c683016a0442c6ae2afee6097d4d99771e155e90
DaltonHart <daltonvaren@gmail.com>
2022-03-16 09:18:09 -0700

    :pencil: (alias) adds alias example with join

--- queries/alias.sql
+++ queries/alias.sql
@@ -6,3 +6,12 @@ SELECT name
 SELECT b.name 
   FROM bands b
   WHERE b.genre='rock';
+
+-- example with a join 
+SELECT 
+  s.title,
+  b.name, 
+  b.genre
+  FROM songs s
+  INNER JOIN bands b
+  ON (s.band_id = b.band_id);
